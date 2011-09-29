c08cf2c781cd86459ece7064c26b101b412fcfb8
Sean Huber <shuber@einsteinindustries.com>
2011-09-28 22:24:24 -0700

    Add request method place holder

--- lib/net/http/last_modified_cache.rb
+++ lib/net/http/last_modified_cache.rb
@@ -1,6 +1,16 @@
 module Net
   class HTTP
     module LastModifiedCache
+      def self.included(base)
+        base.class_eval do
+          alias_method :request_without_last_modified_cache, :request
+          alias_method :request, :request_with_last_modified_cache
+        end
+      end
+
+      def request_with_last_modified_cache(request, body = nil, &block)
+        super
+      end
     end
   end
 end
