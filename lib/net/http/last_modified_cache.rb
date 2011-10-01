cd64b7fe54604dcb06818e49b4ea1fa2df12a73f
Sean Huber <shuber@einsteinindustries.com>
2011-09-30 20:50:20 -0700

    Add #disable! #enable! and #enabled? methods

--- lib/net/http/last_modified_cache.rb
+++ lib/net/http/last_modified_cache.rb
@@ -1,10 +1,24 @@
 module Net
   class HTTP
     module LastModifiedCache
-      def self.included(base)
-        base.class_eval do
-          alias_method :request_without_last_modified_cache, :request
-          alias_method :request, :request_with_last_modified_cache
+      class << self
+        def disable!
+          @enabled = false
+        end
+
+        def enable!
+          @enabled = true
+        end
+
+        def enabled?
+          @enabled ||= enable!
+        end
+
+        def included(base)
+          base.class_eval do
+            alias_method :request_without_last_modified_cache, :request
+            alias_method :request, :request_with_last_modified_cache
+          end
         end
       end
 
