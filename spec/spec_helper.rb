534200c6b733ebcb9d6e080b5e748f4bd0cddc15
Sean Huber <shuber@einsteinindustries.com>
2011-10-01 00:45:18 -0700

    Remove unused rspec support directory loading from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ $:.unshift(File.join(spec_root, '..', 'lib'))
 $:.unshift(spec_root)
 require 'net-http-last_modified_cache'
 
-Dir[File.join(spec_root, 'support/**/*.rb')].each { |file| require file }
-
 RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
