db0ff862ebfd939af34c3eda6ca0e5eded36d6d7
Sean Huber <shuber@einsteinindustries.com>
2011-09-30 23:34:30 -0700

    Add rack dependency for ActiveSupport::Cache

--- lib/net-http-last_modified_cache.rb
+++ lib/net-http-last_modified_cache.rb
@@ -2,5 +2,6 @@ require 'active_support/cache'
 require 'net/http'
 require 'net/http/last_modified_cache'
 require 'net/http/last_modified_cache/version'
+require 'rack/utils'
 
 Net::HTTP.send(:include, Net::HTTP::LastModifiedCache)
--- net-http-last_modified_cache.gemspec
+++ net-http-last_modified_cache.gemspec
@@ -26,4 +26,5 @@ Gem::Specification.new do |s|
 
   s.add_dependency('activesupport')
   s.add_dependency('i18n')
+  s.add_dependency('rack')
 end
